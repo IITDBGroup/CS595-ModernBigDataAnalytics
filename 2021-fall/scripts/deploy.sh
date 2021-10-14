e77954aa352998118ba3d711bcf45cc4b69cce20
Boris Glavic <lordpretzel@gmail.com>
2021-10-14 14:53:05 -0500

    lsm

--- 2021-fall/scripts/deploy.sh
+++ 2021-fall/scripts/deploy.sh
@@ -5,11 +5,15 @@ USER=glavic
 CURL=curl
 SERVER_DIR=/home/glavic/public_html/cs595/2021-fall
 SERVER_FULL=${USER}@${SERVER}:${SERVER_DIR}
-SCRIPTDIR=$(dirname "${BASH_SOURCE}")
+pushd $(dirname "${0}") > /dev/null
+SCRIPTDIR=$(pwd -L)
+popd > /dev/null
 DEPLOYDIR="${SCRIPTDIR}/../_site/"
 
 echo "BUILD WITH RIGHT PREFIX"
+pushd ${SCRIPTDIR}/..
 bundle exec jekyll clean
 bundle exec jekyll build
 echo "DEPLOY ... ${DEPLOYDIR} TO ${SERVER_FULL}"
 ${RSYNC} ${RSYNC_OPTS} "${DEPLOYDIR}" "${SERVER_FULL}"
+popd
